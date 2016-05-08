a7e6436ed6c88827b5f6daaa8e6e1bd3bc3fe9ea
Lyujia Zhang <lyujiazhang@dyn-209-2-222-223.dyn.columbia.edu>
2016-05-08 16:42:36 -0400

    elastic search app update with login

--- ElasticSearchApp/server.js
+++ ElasticSearchApp/server.js
@@ -1,8 +1,15 @@
 var express = require('express');
-var app = express();
+var session = require('express-session');
 var elasticsearch = require('elasticsearch');
-var global_socket;
+var app = express();
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+    secret: 's_secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -15,52 +22,11 @@ var io = require('socket.io').listen(server);
 server.listen(8081, function () {
   console.log('listening on port 8081!');
 });
-// var http = require('http');
-// var fs = require('fs');
-// var elasticsearch = require('elasticsearch');
 
-// function send404Response(response) {
-//     response.writeHead(404, {"Content-Type": "text/plain"});
-//     response.write("Error 404: Page Not Found");
-//     response.end();
-// }
+var global_socket;
+var global_user;
+var sess;
 
-// function onRequest(request, response) {
-//     if (request.method == 'GET' && request.url == '/') {
-//         response.writeHead(200, {"Content-Type": "text/html"});
-//         fs.createReadStream("./twittermap.html").pipe(response);
-//     }else {
-//         send404Response(response);
-//     }
-// }
-// function searchKey(keyword, socket, data) {
-//     var client = new elasticsearch.Client({
-//         host: 'http://search-twittmap-22o4jxxy4ggsm3wsjhsvvyu66q.us-east-1.es.amazonaws.com',
-//         log: 'trace'
-//     });
-
-//     client.search({
-//         index: keyword,
-//         body: {
-//             "from" : 0,
-//             "size" : 400
-//         }
-//     }).then(function (resp) {
-//         var hits = resp.hits.hits;
-//         var result = [];
-//         for (var i = 0; i<hits.length; i++) {
-//             console.log(hits[i]);
-//             var longtitude = hits[i]._source.longtitude;
-//             var latitude = hits[i]._source.latitude;
-//             var item = {lat: latitude, lng: longtitude, usr:hits[i]._source.user, txt:hits[i]._source.text, ul:hits[i]._source.url };
-//             result.push(item);
-//         }
-//         console.log(result);
-//         socket.emit('marks',{message:result, id:data.id});
-//     }, function (err) {
-//         socket.emit('error', err.message);
-//     });
-// }
 
 function search(keyword) {
     var client = new elasticsearch.Client({
@@ -104,36 +70,33 @@ function search(keyword) {
         global_socket.emit('error', err.message);
     });
 }
-// var app = http.createServer(onRequest);
-// console.log("server working...");
 
-// app.listen(8888);
-// var io = require('socket.io').listen(app);
-// io.on('connect',function(socket){
-//     socket.emit('welcome', { message: 'welcome!', id: socket.id });
-
-//     socket.on('keypass',function(data){   
-//         var key = data.message;
-//         searchKey(key,socket,data);
-//     });
-
-// });
 
 app.get('/', function (req, res) {
-   res.sendFile(__dirname + '/GoogleMap.html');
+   res.sendFile(__dirname + '/signin.html');
 });
 
-app.post('/',function(req,res){
-    console.log(req.body);
+app.post('/signin', function (req, res) {
+   sess = req.session;
+   sess.user = req.body.user;
+   res.send('done');
+});
+
+app.get('/map', function (req, res) {
+   sess = req.session;
+  res.render("GoogleMap", {user: sess.user});
+
+});
+
+app.post('/map', function (req, res) {
   var keyword=req.body.keyword;
   var period=req.body.time;
   console.log("Keyword = "+ keyword +", period is "+ period);
   search(keyword);
- // res.sendFile(__dirname + '/GoogleMap.html');
-  //res.end("yes");
+  res.send('done');
 });
+
  
- // var io = require('socket.io').listen(app);
  io.on('connection', function(socket){
    console.log('a user connected');
    global_socket = socket;
